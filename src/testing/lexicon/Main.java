d99125253760317ed405a99a58b939ce48204d36
Pär Törnell <paer@ijob.se>
2021-12-20 19:46:57 +0100

    Initial commit

---
+++ src/testing/lexicon/Main.java
@@ -0,0 +1,21 @@
+package testing.lexicon;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int mYSum;//Delphi Style
+
+        mYSum= 45 + 11;
+	    System.out.println("45 + 11 = "+mYSum );
+
+        mYSum= 12 * 4;
+        System.out.println("12 * 4  = "+mYSum );
+
+        mYSum= 24 / 6;
+        System.out.println("24/6  = "+mYSum );
+
+        mYSum= 55 - 12;
+        System.out.println("55/12  = "+mYSum );
+
+    }
+}
